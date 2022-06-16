1f7fd1cc4e56fb5c882f669955685fae4bf648b5
RafiBrent <105883594+RafiBrent@users.noreply.github.com>
2022-06-16 17:12:02 -0500

    Create run_chimera_remove_tip3p_mac.sh

---
+++ run_chimera_remove_tip3p_mac.sh
@@ -0,0 +1,13 @@
+##Script to run a python script in chimera 1.16 from the Mac terminal.
+
+/Applications/Chimera.app/Contents/MacOS/chimera --nogui Path/To/process_chimera.py
+
+#NOTE: In extremely rare cases (roughly 1 in 5,000) chimera may fail to add hydrogens to a pdb file. In this case, remove that pdb from the dataset and
+#run this script again.
+
+for f in * ; do
+> echo $f
+> sed -i '' 's/H\.t3p/H	/' $f
+> sed -i '' 's/O\.t3p/O\.3  /' $f
+> done
+
