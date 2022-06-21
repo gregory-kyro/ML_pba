ea2445cac48531627c98dc16057b29dcddce098e
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2022-06-21 12:03:16 -0400

    Update parse_PDBbind_data.py

--- Process_Data/parse_PDBbind_data.py
+++ Process_Data/parse_PDBbind_data.py
@@ -16,22 +16,22 @@ def create_dataset(PDBbind_dataset_path, general_set_PDBs_path, refined_set_PDBs
      
   Output:
   1) A cleaned csv containing PDB id, binding affinity, and set (general/refined):
-     'affinity_data_cleaned.csv'
+     'pdb_affinity_data_cleaned.csv'
   """
   
   # load dataset
   data = pd.read_csv(PDBbind_dataset_path)
   
   # write csv of affinity data
-  affinity_data = data[['pdbid','-logKd/Ki']]
-  affinity_data.to_csv('affinity_data.csv')
+  pdb_affinity_data = data[['pdbid','-logKd/Ki']]
+  pdb_affinity_data.to_csv('pdb_affinity_data.csv')
   
   # check for NaNs in affinity data
-  if affinity_data['-logKd/Ki'].isnull().any() != False:
+  if pdb_affinity_data['-logKd/Ki'].isnull().any() != False:
     print('There are NaNs present in affinity data!')
     
-  # create list of PDB id's in affinity_data
-  pdbid_list = list(affinity_data['pdbid'])
+  # create list of PDB id's in pdb_affinity_data
+  pdbid_list = list(pdb_affinity_data['pdbid'])
   
   # remove affinites that do not have structural data
   missing = []
@@ -39,7 +39,7 @@ def create_dataset(PDBbind_dataset_path, general_set_PDBs_path, refined_set_PDBs
     pdb = pdbid_list[i]
     if os.path.isdir(general_set_PDBs_path %pdb)==False and os.path.isdir(refined_set_PDBs_path %pdb)==False:
         missing.append(pdb)
-  affinity_data = affinity_data[~np.in1d(affinity_data['pdbid'], list(missing))]
+  pdb_affinity_data = pdb_affinity_data[~np.in1d(pdb_affinity_data['pdbid'], list(missing))]
 
   # distinguish PDB id's in general and refined sets
   general_dict = {}
@@ -51,20 +51,20 @@ def create_dataset(PDBbind_dataset_path, general_set_PDBs_path, refined_set_PDBs
     if os.path.isdir(refined_set_PDBs_path %pdb)==True:
         refined_dict[pdb] = 'refined'
    
-  # add 'set' column to affinity_data and fill with 'general'/'refined'
-  affinity_data['set'] = np.nan
-  affinity_data.loc[np.in1d(affinity_data['pdbid'], list(general_dict)), 'set'] = 'general'
-  affinity_data.loc[np.in1d(affinity_data['pdbid'], list(refined_dict)), 'set'] = 'refined'
+  # add 'set' column to pdb_affinity_data and fill with 'general'/'refined'
+  pdb_affinity_data['set'] = np.nan
+  pdb_affinity_data.loc[np.in1d(pdb_affinity_data['pdbid'], list(general_dict)), 'set'] = 'general'
+  pdb_affinity_data.loc[np.in1d(pdb_affinity_data['pdbid'], list(refined_dict)), 'set'] = 'refined'
   
   # write out csv of cleaned dataset
-  affinity_data[['pdbid', '-logKd/Ki', 'set']].to_csv('affinity_data_cleaned.csv', index=False)
+  pdb_affinity_data[['pdbid', '-logKd/Ki', 'set']].to_csv('pdb_affinity_data_cleaned.csv', index=False)
   
   # read in and view the cleaned dataset
-  display(pd.read_csv('affinity_data_cleaned.csv'))
+  display(pd.read_csv('pdb_affinity_data_cleaned.csv'))
   
   if plot == True:
     # plot affinity distributions for general and refined sets
-    grid = sns.FacetGrid(affinity_data, row='set', row_order=['general', 'refined'],
+    grid = sns.FacetGrid(pdb_affinity_data, row='set', row_order=['general', 'refined'],
                      size=3, aspect=2)
     grid.map(sns.distplot, '-logKd/Ki')
   else:
