c3473f3773ed86b2caa1b401070a49aa445cb53f
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2022-06-21 13:39:01 -0400

    Delete parse_data_notebook.py

--- Process_Data/parse_data_notebook.py
+++
@@ -1,3 +0,0 @@
-"" create cleaned pdb affinity dataset"""
-from parse_PDBbind_data import create_dataset
-create_dataset('path/to/dataset.csv', 'path/to/general/set/pdbs/%s', 'path/to/refined/set/pdbs/%s', plot=True)
