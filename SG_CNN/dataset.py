bd54774a76b3f0cbb29bedba1c895fddde8da9f9
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2022-06-21 08:29:11 -0400

    Update dataset.py

--- SG_CNN/dataset.py
+++ SG_CNN/dataset.py
@@ -31,3 +31,60 @@ from torch_geometric.nn.inits import uniform, reset
 from torch_geometric.utils import softmax, dense_to_sparse
 from torch_geometric.utils import (to_dense_batch, add_self_loops, remove_self_loops, normalized_cut, dense_to_sparse, 
                                    is_undirected, to_undirected, scatter_, contains_self_loops)
+
+""" Define a class to contain the data that will be included in the dataloader 
+sent to the SG CNN model """
+class SGCNN_Dataset(Dataset):
+    def __init__(self, data_file, output_info=False, cache_data=True):
+        super(SGCNN_Dataset, self).__init__()
+        self.data_file = data_file
+        self.output_info = output_info
+        self.cache_data = cache_data
+        self.data_dict = {}  # used to store data
+        self.data_list = []  # used to store id's for data
+        
+        # retrieve PDB id's and affinities from h5py file
+        with h5py.File(data_file, "r") as f:
+            for pdbid in f.keys():
+                affinity = np.asarray(f[pdbid].attrs["affinity"]).reshape(1, -1)
+                self.data_list.append((pdbid, affinity))  
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, item):
+        if self.cache_data:
+            if item in self.data_dict.keys():
+                return self.data_dict[item]
+            else:
+                pass       
+        pdbid, affinity = self.data_list[item]
+        node_feats, coords = None, None
+        
+        with h5py.File(self.data_file, "r") as f:
+            if (not pdbid in f.keys()):
+                print(pdbid + 'was not found in the data file')
+                return None
+            data = f[pdbid]
+            vdw_radii = (f[pdbid].attrs["van_der_waals"].reshape(-1, 1))
+            coords = data[:, 0:3]
+            node_feats = np.concatenate([vdw_radii, data[:, 3:22]], axis=1)
+        
+        # account for the vdw radii in distance calculations (consider each atom as a sphere)
+        dists = pairwise_distances(coords, metric="euclidean")
+        edge_index, edge_attr = dense_to_sparse(torch.from_numpy(dists).float())
+        x = torch.from_numpy(node_feats).float()
+        y = torch.FloatTensor(affinity).view(-1, 1)
+        data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr.view(-1, 1), y=y)
+        
+        if self.cache_data:
+            if self.output_info:
+                self.data_dict[item] = (pdbid, data)
+            else:
+                self.data_dict[item] = data
+            return self.data_dict[item]
+        else:
+            if self.output_info:
+                return (pdbid, data)
+            else:
+                return data
